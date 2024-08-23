353158c76198a9cc61675f5f49fd3f99e010b16a
THETOMMY1122 <146836946+THETOMMY1122@users.noreply.github.com>
2024-08-23 07:40:45 -0500

    se agregó el readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# CARPETA PRINCIPAL AL CUROS DE PROGRAMACIÓN
+----
+## Subtitulo 
+###### CONTENIDOS
+
+[Link al curso](https://github.com/dmancera227soyudemedellin/introprogramacionMateo)
--- olamundo.py
+++ olamundo.py
@@ -1,1 +1,6 @@
-print("hola")
+num1 = int(input("ingreses un numero"))
+num2 = int(input("ingrese otro numero"))
+if num1 > num2 
+print("tu numero es mayor")
+else num2 > num1 
+print("tu numero es menor")
